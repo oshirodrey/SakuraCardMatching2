fff22b2ad824d924283dc3370d76c038522392a5
OshiroDrey <96662756+oshirodrey@users.noreply.github.com>
2026-04-22 10:04:42 +0700

    complete menu scene navigation

--- Assets/Scripts/UI/MainMenuController.cs
+++ Assets/Scripts/UI/MainMenuController.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 
+
 public class MainMenuController : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -13,4 +14,30 @@ public class MainMenuController : MonoBehaviour
     {
         
     }
+
+    public void openModeSelectScene(){
+        UnityEngine.SceneManagement.SceneManager.LoadScene("ModeChoosing(sing-mul)");
+        
+    }
+    public void openSettingsScene(){
+        UnityEngine.SceneManagement.SceneManager.LoadScene("Settings");
+    }
+    public void openRulesScene(){
+        UnityEngine.SceneManagement.SceneManager.LoadScene("Rules");
+    }
+    public void openHighScoreScene(){
+        UnityEngine.SceneManagement.SceneManager.LoadScene("HighScores");
+    }
+    public void exitGame(){
+        Application.Quit();
+    }
+
+    public void openMainMenuScene(){
+        UnityEngine.SceneManagement.SceneManager.LoadScene("MainMenu");
+    }
+
+    public void openSinglePlayerScene(){
+        UnityEngine.SceneManagement.SceneManager.LoadScene("Singleplayer");
+    }
+
 }
